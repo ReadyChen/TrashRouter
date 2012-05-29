68cda6e961f97e4770c11f4a689efc27549847f2
ReadyAir <readyair@Mac-8.local>
2012-05-29 17:52:27 +0800

    Initial Commit

---
+++ TrashRouter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrashRouter
+//
+//  Created by readyair on 12/5/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrashRouter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TrashRouter
+//
+//  Created by readyair on 12/5/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
